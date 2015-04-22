008d9eeced7db7d3ac7d6c10825e46dc809b4566
Rob <robleewilliams3@gmail.com>
2015-04-21 23:30:02 -0400

    Almost every battle function
    I've written the base functions for all of the things we want to look at
    in battle.
    
    -Must run data through it to test
    -Must write jason converter
    -Time permitting: do post processing with Database

--- battle_parser/battleParser.py
+++ battle_parser/battleParser.py
@@ -5,24 +5,18 @@ battle = {}
 p1a = Player()		
 p2a = Player()
 turn = ""
-weather = dict([("RainDance", 0), ("PrimordialSea", 0), ("SunnyDay", 0), ("DesolateLand", 0), ("Sandstorm", 0), ("Hail", 0), ("DeltaStream", 0)])
+weather = { "RainDance":0, "PrimordialSea":0, "SunnyDay":0, "DesolateLand":0, "Sandstorm":0, "Hail":0, "DeltaStream":0, "none":1}
+
 #p1cur
 #p2cur
 
-#|-weather
-#|RainDance
-#|PrimordialSea
-#|SunnyDay
-#|DesolateLand
-#|Sandstorm
-#|Hail
-#|DeltaStream
 
 class Player:
 	def __init__(self)
 		self.name = ""
 		self.pokemon = {}
 		self.heals = 0
+		self.hazards = {"Spikes":0, "Stealth Rock":0, "Sticky Web":0, "Toxic Spikes":0, "Tailwind":0}
 		
 class Pokemon:
 	def __init__(self, pkname):
@@ -31,7 +25,9 @@ class Pokemon:
 		self.item = ""
 		self.ability = ""
 		self.move = []
-		self.status = {}
+		self.status = {"brn":0, "par":0, "slp":0, "frz":0, "psn":0, "tox":0, "confusion":0, "trapped":0}
+		self.crit = 0
+		
 		
 		
 #This is the parsing function		
@@ -125,6 +121,31 @@ def item(input):
 	else:
 		p2cur.item = input[3]
 
+#|-boost|p2a: Genesect-Burn|spa|1|[from] ability: Download
+def boost(input):
+	if len(input) < 6:
+		if "p1a" in input[2]:
+			if "ability" in input[5]:
+				p1cur.ability = input[5].split(":").lstrip
+		else:
+			if "ability" in input[5]:
+				p2cur.ability = input[5].split(":").lstrip
+	#else: could get stat boosts			
+			
+#|-damage|p1a: Foxheart|244\/271|[from] item: Life Orb
+#|-damage|p1a: Foxheart|184\/271 slp|[from] ability: Bad Dreams	
+def damage(input):
+	if "p1a" in input[2]:
+		if "item" in input[4]:
+			p1cur.item = input[4].split(":").lstrip()
+		elif "ability" in input[4]:
+			p1cur.ability = input[4].split(":").lstrip()
+	else:
+		if "item" in input[4]:
+			p2cur.item = input[4].split(":").lstrip()
+		elif "ability" in input[4]:
+			p2cur.ability = input[4].split(":").lstrip()
+			
 #|-weather|RainDance
 def weather(input):
 	turns = weather[input[2]]
@@ -136,7 +157,10 @@ def turn(input):
 	turn = input[3]
 	
 #|-status|p1a: i built that.|tox
-def status(input):
+#|-status
+#|brn, par, slp, frz, psn, tox, 
+
+def status(input):	
 	if "p1a:" in input[2]:
 		stus = p1cur.status[input[3]]
 		stus += 1
@@ -145,6 +169,24 @@ def status(input):
 		stus = p2cur.status[input[3]]
 		stus += 1
 		p2cur.status[input[3]] = stus
+
+#|-activate
+#|confusion, trapped
+		
+		
+#|-crit|p2a: LCS is on rn
+def crit(input):
+	if "p1a:" in input[2]:
+		p2cur.crit += 1
+	else:
+		p1cur.crit += 1
+
+#|-sidestart|p2: Jibaku|move: Stealth Rock
+def inhaz(input):
+	if "p1" in input[2]:
+		p1a.hazards[input[3].split(":").lstrip()] = 1;
+	else:
+		p1a.hazards[input[3].split(":").lstrip()] = 1;
 
 
 #Battle{
